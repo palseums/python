7c0f81fa88195d4f367a1e846cfac2b40eb94ba6
palseums <57275214+palseums@users.noreply.github.com>
2023-01-12 15:45:20 -0600

    print
    print

---
+++ print/print1.py
@@ -0,0 +1,9 @@
+n = 1
+fl = 2.123456
+name = "palani"
+print("%s" % n)
+print("%1f" % fl)
+print("%1.3f" % fl)
+print("{0},{1}".format(n,fl))
+print("{0},{1:1.3f}".format(n,fl))
+print(f"My name is {name}")
