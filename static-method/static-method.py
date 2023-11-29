2e4cf9377f36290d32f7fefb37f42f5e73f6b2bd
palseums <57275214+palseums@users.noreply.github.com>
2023-11-29 11:44:28 -0600

    Update static-method.py

--- static-method/static-method.py
+++ static-method/static-method.py
@@ -2,6 +2,10 @@ class Person:
     # Class attribute
     name1 = 'palani'
 
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
     @staticmethod
     def access1():
         return "Hello"
@@ -11,3 +15,4 @@ obj = Person()
 #className.static_method_name()
 Person.access1()
 # Static method cannot access class attribute name1 above
+# Static method cannot access instance attribute name and age
