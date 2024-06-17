8c150dc687f7fda17daddd41e9919a3ca81243c4
palseums <57275214+palseums@users.noreply.github.com>
2024-06-17 09:39:42 -0500

    2d travel
    2d travel

---
+++ Array/2d_travel.py
@@ -0,0 +1,23 @@
+region_list = [
+
+    [1,2],
+    [3,4],
+    [5,6]
+
+]
+
+
+
+for r in range(3):
+    for c in range(2):
+        print(region_list[r][c])
+
+
+
+for c in range(2):
+    for r in range(3):
+        print(region_list[r][c])
+
+
+
+
