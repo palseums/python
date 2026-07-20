afeec83de221d6f7b70d8699179d1afa5c652a4e
palani <palaniappan.mca1@gmail.com>
2026-07-20 16:07:57 -0500

    adding some comment

--- Array/2d.py
+++ Array/2d.py
@@ -12,10 +12,13 @@ print(region_list)
 for i in region_list:
     for j in i:
         print(j)
+    print("#########")
 
 print("******************")
 
 # It prints Germany
+# Here 2 is row
+# Here 1 is column
 region_list[2][1]
 
 print(region_list[2][1])
